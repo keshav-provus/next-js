3a859cf3fb2b785ab306527e22e68bdffc8cb4b4
keshav-provus <keshav@provus.ai>
2026-02-17 14:29:39 +0530

    Added Hearder & Footer in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,22 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
+        <header style={{ 
+          backgroundColor: 'blue', 
+          color: 'white', 
+          padding: '1rem' 
+        }}> 
+          Header
+        </header>
         {children}
+
+        <footer style={{ 
+          backgroundColor: 'blue', 
+          color: 'white', 
+          padding: '1rem' 
+        }}> 
+          Footer
+        </footer>
       </body>
     </html>
   )
